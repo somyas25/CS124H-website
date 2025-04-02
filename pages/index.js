fc47adf5001ec81f6479df46ef965639ae73ff38
Somya <somyas3@illinois.edu>
2025-04-02 18:09:25 -0500

    Initial Setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>Welcome to My Next.js Website</h1>
+        <p>This is the home page.</p>
+      </main>
+    </div>
+  );
+}
