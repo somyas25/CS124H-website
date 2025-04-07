9302eb606e1af552fccf9bdd094c6b0639219ced
milocoder04011 <milocoder01@gmail.com>
2025-04-07 00:43:05 -0500

    Fixed nav bar & made timeline UI

--- app/timeline/page.js
+++ app/timeline/page.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Navbar from '../../components/navbar.js';
+import styles from '../../styles/Timeline.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
         <Navbar/>
-        <main>
-            <h1>Welcome to the Timeline</h1>
-            <p>This is a dummy page.</p>
+        <main className={styles.main}>
+        <div className={styles.boxWrapper}>
+            <div className={styles.whiteBox}>
+              <h2>Add our GCAL <a href="https://www.google.com/calendar" target="_blank">here</a></h2>
+            </div>
+            <div className={styles.greyBox}>
+              <h2>Upcoming Events</h2>
+                <div className={styles.whiteBoxMini}>
+                  <p>This is the mini event 1 white box.</p>
+                </div>
+
+                <div className={styles.whiteBoxMini}>
+                    <p>This is the mini event 2 white box.</p>
+                </div>
+
+                <div className={styles.whiteBoxMini}>
+                    <p>This is the mini event 3 white box.</p>
+                </div>
+            </div>
+          </div>
         </main>
     </div>
   );
