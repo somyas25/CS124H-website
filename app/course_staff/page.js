1890aa69657dccf95fcfd538ceda65620291ac0e
Somya <somyas3@illinois.edu>
2025-04-09 18:23:36 -0500

    Added global css

--- app/course_staff/page.js
+++ app/course_staff/page.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navbar from '../../components/navbar.js';
+import '../../styles/CourseStaff.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className="main-content">
         <Navbar/>
         <main>
             <h1>Welcome to Course Staff Page</h1>
             <p>This is a dummy page.</p>
+            <p>This is a dummy page.</p>
+
         </main>
     </div>
   );
