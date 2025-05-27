34cf491089d5ed42cc03f588928d67d61a7ed4bc
Somya <somyas3@illinois.edu>
2025-05-26 18:44:18 -0700

    Fixed some styling

--- app/timeline/page.js
+++ app/timeline/page.js
@@ -9,7 +9,7 @@ export default function Home() {
         <main className={styles.main}>
         <div className={styles.boxWrapper}>
             <div className={styles.whiteBox}>
-              <h2>Add our GCAL <a href="https://calendar.google.com/calendar/embed?src=cs124honors%40gmail.com&ctz=America%2FChicago" target="_blank">here</a></h2>
+              <h2 className='gcal-header'>Add our GCAL <a href="https://calendar.google.com/calendar/embed?src=cs124honors%40gmail.com&ctz=America%2FChicago" target="_blank">here</a></h2>
               <div style={{ display: "flex", justifyContent: "center", align: "center", height: "85%", width: "100%"}}>
                 {/* add &mode=WEEK at the end of the url to change to week view */}
                 <iframe src="https://calendar.google.com/calendar/embed?src=cs124honors%40gmail.com&ctz=America%2FChicago" 
@@ -23,7 +23,7 @@ export default function Home() {
             </div>
             <div className={styles.greyBox}>
               <h2>Upcoming Events</h2>
-                <div className={styles.whiteBoxMini}>
+                {/* <div className={styles.whiteBoxMini}>
                   <p className={styles.eventBoxText}>This is the mini event 1 white box.</p>
                 </div>
 
@@ -33,7 +33,7 @@ export default function Home() {
 
                 <div className={styles.whiteBoxMini}>
                     <p className={styles.eventBoxText}>This is the mini event 3 white box.</p>
-                </div>
+                </div> */}
             </div>
           </div>
         </main>
